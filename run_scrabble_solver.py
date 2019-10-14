337299ec5b2c9c4eeabaa6c9f113314084ef9ab5
Sam Slate <sslate727@gmail.com>
2019-10-13 23:28:59 -0400

    updated module name

--- run_scrabble_solver.py
+++ run_scrabble_solver.py
@@ -5,7 +5,7 @@
 #
 # Uses dawg implementation by Steve Hanov at http://stevehanov.ca/blog/?id=115
 
-import scrabble_solver_game_without_debug
+import scrabble_solver_game
 from helper_lists import SCRABBLE_HEIGHT, SCRABBLE_WIDTH, DOUBLE_LETTER, TRIPLE_LETTER, DOUBLE_WORD, TRIPLE_WORD, BLANK, SCRABBLE_BINGO_BONUS, SCRABBLE_BONUS_PLACEMENTS, SCRABBLE_LETTER_POINTS, DICTIONARY
 
 placed_tiles_1 = {
@@ -23,7 +23,7 @@ current_rack_1 = ["a", "v", "e", "r", "f", "u", "m"]
 
 
 if __name__ == "__main__":
-	game = scrabble_solver_game_without_debug.Game(SCRABBLE_HEIGHT, SCRABBLE_WIDTH, placed_tiles_1, SCRABBLE_BONUS_PLACEMENTS, SCRABBLE_LETTER_POINTS, current_rack_1, SCRABBLE_BINGO_BONUS, DICTIONARY)
+	game = scrabble_solver_game.Game(SCRABBLE_HEIGHT, SCRABBLE_WIDTH, placed_tiles_1, SCRABBLE_BONUS_PLACEMENTS, SCRABBLE_LETTER_POINTS, current_rack_1, SCRABBLE_BINGO_BONUS, DICTIONARY)
 	game.Algorithm()
 	game.print_possible_words()
 	game.print_highest()
